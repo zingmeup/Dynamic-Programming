dd80dd44e54451979f8d43cc3f0a7d1a5871ef07
Deepak Yadav <34516539+deepakyadav23@users.noreply.github.com>
2019-07-08 13:12:07 +0530

    binomial

---
+++ src/binomial_coefficients/BinomialCoefficient.java
@@ -0,0 +1,26 @@
+package binomial_coefficients;
+
+public class BinomialCoefficient {
+    public static void main(String[] args) {
+//        int n=4,k=2;
+//        int[][] coeff=new int[n+1][k+1];
+//        for (int i = 0; i <n ; i++) {
+//            for (int j = 0; j <Math.min(i,k) ; j++) {
+//                System.out.println("coef "+i+" "+j);
+//                if (j==0||j==i){
+//                    coeff[i][j]=1;
+//                }else{
+//                    coeff[i][j]=coeff[i-1][j-1]+coeff[i-1][j];
+//                }
+//            }
+//        }
+//        for (int i = 0; i <=4; i++) {
+//            for (int j = 0; j <=2 ; j++) {
+//                System.out.print(" "+coeff[i][j]);
+//            }
+//            System.out.println();
+//        }
+//
+
+    }
+}
